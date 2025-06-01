e1bbb6bab5cf7ba2c78c8698a594db7cd4fe06a7
Anton Örn Karlsson <anton.orn.karlsson@gmail.com>
2025-06-01 21:44:34 +0000

    Add React app structure and build configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ancient History Educational App</h1>
+      <p>Welcome to the app!</p>
+    </div>
+  );
+}
+
+export default App;
